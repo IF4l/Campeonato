174e478755e1fd9b90c484c1cc003593213bc6e8
leticia.nunes1 <leticia.nunes1@FLE207D30.alunos.sc.senac.br>
2021-11-19 11:51:53 -0300

    Atualização da stressadinha da classe usuário dao impl

--- src/br/com/senac/dao/UsuarioDaoImpl.java
+++ src/br/com/senac/dao/UsuarioDaoImpl.java
@@ -23,131 +23,120 @@ public class UsuarioDaoImpl {
     private Connection conexao;
     private PreparedStatement prepararSql;
     private ResultSet resultado;
-    
-    
-    public Usuario logar(String login, String senha){
+
+    public Usuario logar(String login, String senha) {
         String consulta = "SELECT * FROM usuario WHERE login = ? and senha = ?";
         Usuario usuario = null;
         try {
             conexao = FabricaConexao.abrirConexao();
             prepararSql = conexao.prepareStatement(consulta);
+          
             prepararSql.setString(1, login);
             prepararSql.setString(2, senha);
             resultado = prepararSql.executeQuery();
             if (resultado.next()) {
                 usuario = new Usuario();
-                usuario.setId(id);
+                usuario.setId(resultado.getInt("id"));
                 usuario.setNome(resultado.getString("nome"));
-                usuario.setNascimento(resultado.getDate("nascimento"));
-                usuario.setBola(resultado.getString("bola"));
+                usuario.setLogin(resultado.getString("login"));
+                usuario.setSenha(resultado.getString("senha"));
             }
 
         } catch (Exception e) {
-            System.out.println("Erro ao pesquisar jogador por id" + e.getMessage());
+            System.out.println("Erro ao pesquisar usuario por id" + e.getMessage());
         }
         return usuario;
     }
 
-    public List<Jogador> pesquisarPorNome(String nome) {
-        String consulta = "SELECT * FROM jogador WHERE nome LIKE ?";
-        Jogador jog;
-        List<Jogador> jogadores = new ArrayList<>();
+    public List<Usuario> pesquisarPorNome(String nome) {
+        String consulta = "SELECT * FROM usuario WHERE nome LIKE ?";
+        Usuario usuario;
+        List<Usuario> usuarios = new ArrayList<>();
         try {
             conexao = FabricaConexao.abrirConexao();
             prepararSql = conexao.prepareStatement(consulta);
             prepararSql.setString(1, "%" + nome + "%");
             resultado = prepararSql.executeQuery();
             while (resultado.next()) {
-                jog = new Jogador();
-                jog.setId(resultado.getInt("id"));
-                jog.setNome(resultado.getString("nome"));
-                jog.setNascimento(resultado.getDate("nascimento"));
-                jog.setBola(resultado.getString("bola"));
-                jog.setTelefone(resultado.getString("Telefone"));
-                jog.setEmail(resultado.getString("email"));
-                jog.setFederacao(resultado.getString("federacao"));
-                jogadores.add(jog);
+                usuario = new Usuario();
+                usuario.setNome(resultado.getString("nome"));
+                usuario.setLogin(resultado.getString("login"));
+                usuario.setSenha(resultado.getString("senha"));
+                usuarios.add(usuario);
             }
 
         } catch (Exception e) {
             System.out.println("Erro ao pesquisar jogador por nome " + e.getMessage());
         }
 
-        return jogadores;
+        return usuarios;
     }
 
-    public void salvar(Jogador jogador) {
-        String sql = "INSERT INTO jogador (nome, nascimento, bola, telefone, email, federacao)"
-                + " values(?, ?, ?, ?, ?, ?)";
+    public void salvar(Usuario usuario) {
+        String sql = "INSERT INTO usuario (nome,login, senha)"
+                + " values(?, ?, ?)";
         try {
             conexao = FabricaConexao.abrirConexao();
             prepararSql = conexao.prepareStatement(sql);
-            prepararSql.setString(1, jogador.getNome());
-            prepararSql.setDate(2,  new Date(jogador.getNascimento().getTime()));
-            prepararSql.setString(3, jogador.getBola());
-            prepararSql.setString(4, jogador.getTelefone()); 
-            prepararSql.setString(5, jogador.getEmail());
-            prepararSql.setString(6, jogador.getFederacao());
+            prepararSql.setString(1, usuario.getNome());
+            prepararSql.setString(2, usuario.getLogin());
+            prepararSql.setString(3, usuario.getSenha());
+          
             prepararSql.executeUpdate();
 
         } catch (Exception e) {
-            System.out.println("Erro ao salvar o jogador " + e.getMessage());
+            System.out.println("Erro ao salvar o usuario " + e.getMessage());
         }
     }
 
     public void excluir(int id) {
         try {
             conexao = FabricaConexao.abrirConexao();
-            prepararSql = conexao.prepareStatement("DELETE FROM jogador WHERE id = ?");
+            prepararSql = conexao.prepareStatement("DELETE FROM usuario WHERE id = ?");
             prepararSql.setInt(1, id);
             prepararSql.executeUpdate();
 
         } catch (Exception e) {
-            System.out.println("erro ao excluir jogador " + e.getMessage());
+            System.out.println("erro ao excluir usuario " + e.getMessage());
         }
     }
 
-    public void alterar(Jogador jogador) {
-        String sql = "UPDATE jogador SET nome = ?, nascimento = ?, bola= ?, telefone = ?, email = ?, federacao = ? WHERE id = ?";
+    public void alterar(Usuario usuario) {
+        String sql = "UPDATE usuario SET nome = ?, login = ?,  senha = ? WHERE id = ?";
         try {
             conexao = FabricaConexao.abrirConexao();
             prepararSql = conexao.prepareStatement(sql);
-            prepararSql.setString(1, jogador.getNome());
-            prepararSql.setDate(2, new Date(jogador.getNascimento().getTime()));
-            prepararSql.setString(3, jogador.getBola());
-            prepararSql.setString(4, jogador.getEmail());
-            prepararSql.setString(5, jogador.getTelefone());
-            prepararSql.setString(6, jogador.getFederacao());
-            prepararSql.setInt(7, jogador.getId());
+            prepararSql.setString(1, usuario.getNome());
+            prepararSql.setString(2, usuario.getLogin());
+            prepararSql.setString(3, usuario.getSenha());
+            
+            prepararSql.setInt(7, usuario.getId());
             prepararSql.executeUpdate();
 
         } catch (Exception e) {
-            System.out.println("Erro ao alterar o jogador " + e.getMessage());
+            System.out.println("Erro ao alterar o usuario " + e.getMessage());
         }
     }
 
-    public Jogador pesquisarPorID(int id) {
-        String consulta = "SELECT * FROM jogador WHERE id = ?";
-        Jogador jogador = null;
+    public Usuario pesquisarPorID(int id) {
+        String consulta = "SELECT * FROM usuario WHERE id = ?";
+        Usuario usuario = null;
         try {
             conexao = FabricaConexao.abrirConexao();
             prepararSql = conexao.prepareStatement(consulta);
             prepararSql.setInt(1, id);
             resultado = prepararSql.executeQuery();
             if (resultado.next()) {
-                jogador = new Jogador();
-                jogador.setId(id);
-                jogador.setNome(resultado.getString("nome"));
-                jogador.setNascimento(resultado.getDate("nascimento"));
-                jogador.setBola(resultado.getString("bola"));
-                jogador.setTelefone(resultado.getString("telefone"));
-                jogador.setEmail(resultado.getString("email"));
-                jogador.setFederacao(resultado.getString("federacao"));
+                usuario = new Usuario();
+                usuario.setId(id);
+                usuario.setNome(resultado.getString("nome"));
+                usuario.setLogin(resultado.getString("login"));
+                usuario.setSenha(resultado.getString("senha"));
             }
 
         } catch (Exception e) {
-            System.out.println("Erro ao pesquisar jogador por id" + e.getMessage());
+            System.out.println("Erro ao pesquisar usuario por id" + e.getMessage());
         }
-        return jogador;
+        return usuario;
     }
 }
